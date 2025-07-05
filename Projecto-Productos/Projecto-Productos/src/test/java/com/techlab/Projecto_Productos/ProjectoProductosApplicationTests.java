38083b44ceb98a826c63c05f5363ca99a8cbfd51
Colby <cvertilus@fi.uba.ar>
2025-07-05 20:55:11 -0300

    manenjo de try catch en las entradas , para mejor rendimiento

---
+++ Projecto-Productos/Projecto-Productos/src/test/java/com/techlab/Projecto_Productos/ProjectoProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlab.Projecto_Productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
